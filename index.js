6d3d8d08dad4bb5be18185c84dbcdd22b510391c
Rursky <71942106+Rursky@users.noreply.github.com>
2020-10-14 18:27:48 +0200

    Update index.js
    Answers for req 5 and 6 added

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-// //Zadanie 1
+//Zadanie 1
 
-// function TellNumber () {
-//     let number1 = prompt("Podaj jakis numer:", "1");
-//     if (number1 % 2 == 0)
-//         alert("Jest parzysta")
-//     else
-//         alert("Nie jest parzysta")
-// }
+function TellNumber () {
+    let number1 = prompt("Podaj jakis numer:", "1");
+    if (number1 % 2 == 0)
+        alert("Jest parzysta")
+    else
+        alert("Nie jest parzysta")
+}
 
-// TellNumber()
+TellNumber()
 
-// //Zadanie 2 (możliwość 1)
+//Zadanie 2 (możliwość 1)
 
-// function TellNumber1 () {
-//     let number2 = prompt("Podaj jakis numer:", "1");
-//     if (number2>=-35 && number2<2)
-//         alert("Jest w przedziale")
-//     else if (number2>11 && number2<=27)
-//         alert("Jest w przedziale")
-//     else
-//         alert("Nie jest w przedziale")
-// }
+function TellNumber1 () {
+    let number2 = prompt("Podaj jakis numer:", "1");
+    if (number2>=-35 && number2<2)
+        alert("Jest w przedziale")
+    else if (number2>11 && number2<=27)
+        alert("Jest w przedziale")
+    else
+        alert("Nie jest w przedziale")
+}
 
-// TellNumber1()
+TellNumber1()
 
-// //Zadanie 2 (możliwość 2)
+//Zadanie 2 (możliwość 2)
 
-// function TellNumber3 () {
-//     let number2 = prompt("Podaj jakis numer:", "1");
-//     if ((number2>=-35 && number2<2) || (number2>11 && number2<=27))
-//         alert("Jest w przedziale")
-//     else
-//         alert("Nie jest w przedziale")
-// }
+function TellNumber3 () {
+    let number2 = prompt("Podaj jakis numer:", "1");
+    if ((number2>=-35 && number2<2) || (number2>11 && number2<=27))
+        alert("Jest w przedziale")
+    else
+        alert("Nie jest w przedziale")
+}
 
-// TellNumber3()
+TellNumber3()
 
-//Zadanie 3.
+Zadanie 3.
 
-// function TellNumber4 () {
-//     let number4 = prompt("Podaj jakis numer:", "1");
-//     parseInt(number4, 10) // przekonwertowałem na dzisietny, ale niewiele to zmienia dalej w "switch()"" tj. ze string byłoby tak samo - o to chodziło?
-//     switch (number4) {
-//         case '1':
-//             alert("Zdałeś egzamin")
-//             break;
-//         case '2':
-//             alert("Zaliczona tylko teoria")
-//             break;
-//         case '3':
-//             alert("Zaliczona tylko praktyka")
-//             break;
-//         case '4':
-//             alert("Egzamin do poprawy")
-//             break;
-//     }
-// }
+function TellNumber4 () {
+    let number4 = prompt("Podaj jakis numer:", "1");
+    parseInt(number4, 10) // przekonwertowałem na dzisietny, ale niewiele to zmienia dalej w "switch()"" tj. ze string byłoby tak samo - czy o to chodziło?
+    switch (number4) {
+        case '1':
+            alert("Zdałeś egzamin")
+            break;
+        case '2':
+            alert("Zaliczona tylko teoria")
+            break;
+        case '3':
+            alert("Zaliczona tylko praktyka")
+            break;
+        case '4':
+            alert("Egzamin do poprawy")
+            break;
+    }
+}
 
-// TellNumber4()
+TellNumber4()
 
 //Zadanie 4.
 
@@ -65,3 +65,17 @@ function saySomething(name){
     alert ("something " + name)
 }
 saySomething("is blue")
+
+//Zadanie 5.
+
+function footballPoints(wygrane, remisy, porazki){
+    alert(wygrane * 3 + remisy * 2 + porazki * 0)
+}
+footballPoints(10, 20, 5)
+
+//Zadanie 6.
+
+function animals(kurczak, krowa, swinia){
+    alert(kurczak * 2 + krowa * 4 + swinia * 4)
+}
+animals(13, 3, 5)
