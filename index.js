9e12b640e0d7a08cc58a749b416ab384f565cd0a
Rursky <71942106+Rursky@users.noreply.github.com>
2020-10-15 14:31:39 +0200

    Update index.js
    Every function one "return" added

--- index.js
+++ index.js
@@ -2,80 +2,88 @@
 
 function TellNumber () {
     let number1 = prompt("Podaj jakis numer:", "1");
+    let answer;
     if (number1 % 2 == 0)
-        alert("Jest parzysta")
+        answer = "Jest parzysta"
     else
-        alert("Nie jest parzysta")
+        answer = "Nie jest parzysta"
+return answer
 }
 
-TellNumber()
+alert(TellNumber())
 
 //Zadanie 2 (możliwość 1)
 
 function TellNumber1 () {
     let number2 = prompt("Podaj jakis numer:", "1");
+    let answer;
     if (number2>=-35 && number2<2)
-        alert("Jest w przedziale")
+        answer = "Jest w przedziale"
     else if (number2>11 && number2<=27)
-        alert("Jest w przedziale")
+        answer = "Jest w przedziale"
     else
-        alert("Nie jest w przedziale")
+        answer = "Nie jest w przedziale"
+return answer
 }
 
-TellNumber1()
+alert(TellNumber1())
 
 //Zadanie 2 (możliwość 2)
 
 function TellNumber3 () {
     let number2 = prompt("Podaj jakis numer:", "1");
+    let answer;
     if ((number2>=-35 && number2<2) || (number2>11 && number2<=27))
-        alert("Jest w przedziale")
+        answer = "Jest w przedziale"
     else
-        alert("Nie jest w przedziale")
+        answer = "Nie jest w przedziale"
+return answer
 }
+alert(TellNumber3())
 
-TellNumber3()
 
-Zadanie 3.
+//Zadanie 3.
 
 function TellNumber4 () {
     let number4 = prompt("Podaj jakis numer:", "1");
     parseInt(number4, 10) // przekonwertowałem na dzisietny, ale niewiele to zmienia dalej w "switch()"" tj. ze string byłoby tak samo - czy o to chodziło?
+    let answer;
     switch (number4) {
         case '1':
-            alert("Zdałeś egzamin")
+            answer = "Zdałeś egzamin"
             break;
         case '2':
-            alert("Zaliczona tylko teoria")
+            answer = "Zaliczona tylko teoria"
             break;
         case '3':
-            alert("Zaliczona tylko praktyka")
+            answer = "Zaliczona tylko praktyka"
             break;
         case '4':
-            alert("Egzamin do poprawy")
+            answer = "Egzamin do poprawy"
             break;
     }
+    return answer
 }
 
-TellNumber4()
+alert(TellNumber4())
 
 //Zadanie 4.
 
 function saySomething(name){
-    alert ("something " + name)
+    return("something " + name)
 }
-saySomething("is blue")
+alert(saySomething("is blue"))
 
 //Zadanie 5.
 
 function footballPoints(wygrane, remisy, porazki){
-    alert(wygrane * 3 + remisy * 2 + porazki * 0)
+    return(wygrane * 3 + remisy * 2 + porazki * 0)
 }
-footballPoints(10, 20, 5)
+alert(footballPoints(10, 20, 5))
 
 //Zadanie 6.
 
 function animals(kurczak, krowa, swinia){
-    alert(kurczak * 2 + krowa * 4 + swinia * 4)
+    return(kurczak * 2 + krowa * 4 + swinia * 4)
 }
-animals(13, 3, 5)
+alert(animals(13, 3, 5))
